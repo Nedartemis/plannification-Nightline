c27010f46eb561abdb44cb7d6c6135f0e9ba0be6
Sacha Hibon <sachaedgar@yahoo.fr>
2025-07-13 01:14:06 +0200

    feat: vars.py

---
+++ src/vars.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+import docs as docs_module
+
+# import tmp as tmp_module
+
+# PATH_TMP = Path(tmp_module.__file__).parent
+PATH_DOCS = Path(docs_module.__file__).parent
+PATH_DOCS_PLANNING_MAY = PATH_DOCS / "05 - Dispos de mai 25 - Paris.xlsx"
